73308d4043eecccbc9f2c9245408ab001aaa4534
MatteusSouza <61070878+MatteusSouza@users.noreply.github.com>
2021-06-15 20:47:48 -0300

    connected data

--- app/src/main/java/com/mattsouza/learnroom/MainActivity.kt
+++ app/src/main/java/com/mattsouza/learnroom/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.mattsouza.learnroom
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val newWordActivityRequestCode = 1
+    private val countryViewModel: CountryViewModel by viewModels {
+        CountryViewModelFactory((application as CountriesApplication).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +26,33 @@ class MainActivity : AppCompatActivity() {
         val adapter = CountryListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        countryViewModel.allCountries.observe(this, Observer { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let { adapter.submitList(it) }
+        })
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewCountryActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(NewCountryActivity.EXTRA_REPLY)?.let {
+                val country = Country(it)
+                countryViewModel.insert(country)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                R.string.empty_not_saved,
+                Toast.LENGTH_LONG).show()
+        }
     }
 }
