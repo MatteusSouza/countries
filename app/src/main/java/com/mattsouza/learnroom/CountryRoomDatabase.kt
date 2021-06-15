1eeca0b4383b2052a88c3b02c7ca68fe9e314d09
MatteusSouza <61070878+MatteusSouza@users.noreply.github.com>
2021-06-15 19:45:51 -0300

    Populate the database

--- app/src/main/java/com/mattsouza/learnroom/CountryRoomDatabase.kt
+++ app/src/main/java/com/mattsouza/learnroom/CountryRoomDatabase.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class CountryRoomDatabase {
     @Database(entities = arrayOf(Country::class), version = 1, exportSchema = false)
@@ -17,7 +20,10 @@ class CountryRoomDatabase {
             @Volatile
             private var INSTANCE: CountryRoomDatabase? = null
 
-            fun getDatabase(context: Context): CountryRoomDatabase {
+            fun getDatabase(
+                context: Context,
+                scope: CoroutineScope
+            ): CountryRoomDatabase {
                 // if the INSTANCE is not null, then return it,
                 // if it is, then create the database
                 return INSTANCE ?: synchronized(this) {
@@ -25,11 +31,44 @@ class CountryRoomDatabase {
                         context.applicationContext,
                         CountryRoomDatabase::class.java,
                         "country_database"
-                    ).build()
+                    )
+                        .addCallback(CountryDatabaseCallback(scope))
+                        .build()
                     INSTANCE = instance
                     // return instance
                     instance
                 }
+            }
+        }
+
+        private class CountryDatabaseCallback(
+            private val scope: CoroutineScope
+        ) : RoomDatabase.Callback() {
+
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                INSTANCE?.let { database ->
+                    scope.launch {
+                        populateDatabase(database.countryDao())
+                    }
+                }
+            }
+
+            suspend fun populateDatabase(countryDao: CountryDao) {
+                // Delete all content here.
+                countryDao.deleteAll()
+
+                // Add some country name.
+                var country  = Country("Brazil")
+                countryDao.insert(country)
+                country = Country("Norway")
+                countryDao.insert(country)
+                country = Country("Netherlands")
+                countryDao.insert(country)
+                country = Country("Canada")
+                countryDao.insert(country)
+
+                // TODO: Add your own words!
             }
         }
     }
