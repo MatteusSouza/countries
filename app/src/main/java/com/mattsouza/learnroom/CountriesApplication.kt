a645b3a3df807fe0e81ed8dddb1cfeb5787a5e34
MatteusSouza <61070878+MatteusSouza@users.noreply.github.com>
2021-06-15 21:04:02 -0300

    Fixed: AndroidRuntime: FATAL EXCEPTION: main; CountryRoomDatabase was duplicated within the scope itself causing the error: 'cannot find implementation for CountryRoomDatabase.CountryRoomDatabase ... '.

--- app/src/main/java/com/mattsouza/learnroom/CountriesApplication.kt
+++ app/src/main/java/com/mattsouza/learnroom/CountriesApplication.kt
@@ -10,6 +10,6 @@ class CountriesApplication : Application() {
 
     // Using by lazy so the database and the repository are only created when they're needed
     // rather than when the application starts
-    val database by lazy { CountryRoomDatabase.CountryRoomDatabase.getDatabase(this, applicationScope) }
+    val database by lazy { CountryRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { CountryRepository(database.countryDao()) }
 }
--- app/src/main/java/com/mattsouza/learnroom/CountryRoomDatabase.kt
+++ app/src/main/java/com/mattsouza/learnroom/CountryRoomDatabase.kt
@@ -8,68 +8,67 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-class CountryRoomDatabase {
-    @Database(entities = arrayOf(Country::class), version = 1, exportSchema = false)
-    public abstract class CountryRoomDatabase : RoomDatabase() {
 
-        abstract fun countryDao(): CountryDao
+@Database(entities = arrayOf(Country::class), version = 1, exportSchema = false)
+public abstract class CountryRoomDatabase : RoomDatabase() {
 
-        companion object {
-            // Singleton prevents multiple instances of database opening at the
-            // same time.
-            @Volatile
-            private var INSTANCE: CountryRoomDatabase? = null
+    abstract fun countryDao(): CountryDao
 
-            fun getDatabase(
-                context: Context,
-                scope: CoroutineScope
-            ): CountryRoomDatabase {
-                // if the INSTANCE is not null, then return it,
-                // if it is, then create the database
-                return INSTANCE ?: synchronized(this) {
-                    val instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        CountryRoomDatabase::class.java,
-                        "country_database"
-                    )
-                        .addCallback(CountryDatabaseCallback(scope))
-                        .build()
-                    INSTANCE = instance
-                    // return instance
-                    instance
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: CountryRoomDatabase? = null
+
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope
+        ): CountryRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    CountryRoomDatabase::class.java,
+                    "country_database"
+                )
+                    .addCallback(CountryDatabaseCallback(scope))
+                    .build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+
+    private class CountryDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.countryDao())
                 }
             }
         }
 
-        private class CountryDatabaseCallback(
-            private val scope: CoroutineScope
-        ) : RoomDatabase.Callback() {
+        suspend fun populateDatabase(countryDao: CountryDao) {
+            // Delete all content here.
+            countryDao.deleteAll()
 
-            override fun onCreate(db: SupportSQLiteDatabase) {
-                super.onCreate(db)
-                INSTANCE?.let { database ->
-                    scope.launch {
-                        populateDatabase(database.countryDao())
-                    }
-                }
-            }
+            // Add some country name.
+            var country  = Country("Brazil")
+            countryDao.insert(country)
+            country = Country("Norway")
+            countryDao.insert(country)
+            country = Country("Netherlands")
+            countryDao.insert(country)
+            country = Country("Canada")
+            countryDao.insert(country)
 
-            suspend fun populateDatabase(countryDao: CountryDao) {
-                // Delete all content here.
-                countryDao.deleteAll()
-
-                // Add some country name.
-                var country  = Country("Brazil")
-                countryDao.insert(country)
-                country = Country("Norway")
-                countryDao.insert(country)
-                country = Country("Netherlands")
-                countryDao.insert(country)
-                country = Country("Canada")
-                countryDao.insert(country)
-
-                // TODO: Add your own words!
-            }
+            // TODO: Add your own words!
         }
     }
 }
